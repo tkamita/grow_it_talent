f68ad584168f4ecfae3336f8a0d62abf2bf4926e
嘉味田崇 <kamitatakashi@kamidatakashinoMacBook-Air.local>
2020-07-21 23:42:34 +0900

    user top画面作成/scss読み込み

---
+++ app/controllers/user/homes_controller.rb
@@ -0,0 +1,7 @@
+class User::HomesController < ApplicationController
+
+  def top
+    
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  namespace :user do
+    get 'top' => 'homes#top', as: 'top'
+  end
+  
+  
 end
