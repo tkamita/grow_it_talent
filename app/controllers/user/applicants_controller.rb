f71cd9646ae8eb615812ea900cd29d67c6849458
嘉味田崇 <kamitatakashi@kamidatakashinoMacBook-Air.local>
2020-07-22 17:44:00 +0900

    update user applicant controller

--- app/controllers/user/applicants_controller.rb
+++ app/controllers/user/applicants_controller.rb
@@ -6,4 +6,19 @@ class User::ApplicantsController < ApplicationController
   def edit
     @applicant = current_applicant
   end
+
+  def update
+    @applicant = current_applicant
+		if @applicant.update(applicant_params)
+			redirect_to user_applicants_path, notice: 'successfully updated'
+		else
+			render action: :edit
+    end
+  end
+
+  private
+  def applicant_params
+    # permit後でいれる
+    params.require(:applicant).permit(:hoge)
+  end
 end
