8bda295bbfb43472ea1fddec30f6d897b2e08903
嘉味田崇 <kamitatakashi@kamidatakashinoMacBook-Air.local>
2020-07-22 15:07:40 +0900

    add applicant/attachment-image

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -4,6 +4,8 @@ class Applicant < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  attachment :image
+
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_furigana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
