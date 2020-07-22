de2c9998fd72d47ec9c77111bf08ca8251fdc5ac
嘉味田崇 <kamitatakashi@kamidatakashinoMacBook-Air.local>
2020-07-22 13:54:36 +0900

    ユーザー新規登録　バリデーション追加

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -3,4 +3,11 @@ class Applicant < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_furigana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_furigana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :email, presence: true, uniqueness: true
+
 end
