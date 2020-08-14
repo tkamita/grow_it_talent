abb282390fa52e442219c69aa2ec662f30830b2f
嘉味田崇 <kamitatakashi@kamidatakashinoMacBook-Air.local>
2020-08-14 19:40:36 +0900

    env

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ unless Rails.env.development? || Rails.env.test?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV[''],
-      aws_secret_access_key: ENV[''],
+      aws_access_key_id: ENV['AWS_ACCESS_KEY'],
+      aws_secret_access_key: ENV['AWS_SECRET_KEY'],
       region: 'ap-northeast-1'
     }
 
