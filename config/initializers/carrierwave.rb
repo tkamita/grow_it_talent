8a59dd137fde2f5d0db12bd9d4e2b6b291f38065
嘉味田崇 <kamitatakashi@kamidatakashinoMacBook-Air.local>
2020-08-19 18:34:39 +0900

    fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,11 +4,11 @@ unless Rails.env.development? || Rails.env.test?
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY'],
       aws_secret_access_key: ENV['AWS_SECRET_KEY'],
-      region: ENV['REGION']
+      region: 'ap-northeast-1'
     }
 
-    config.fog_directory = ENV['S3_BUCKET']
+    config.fog_directory = 'grow-it-talent'
     config.cache_storage = :fog
-    # config.asset_host = ENV['CLOUD_FRONT']
+    config.asset_host = 'http://static.grow-it-talent.work'
   end
 end
